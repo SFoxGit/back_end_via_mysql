e9abb890b10c0839a7dbeff8b918d5a4e6a52768
Shawn Fox <foxeasttrojan06@gmail.com>
2021-03-20 21:48:31 -0400

    fixed decimal, associations complete

--- models/Product.js
+++ models/Product.js
@@ -14,7 +14,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true,
